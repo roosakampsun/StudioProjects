1abf43a0fc0a0a8906716d98d173268852b800ff
roosakampsun <82703352+roosakampsun@users.noreply.github.com>
2021-11-25 23:01:09 +0200

    Add files via upload

--- theme.dart
+++ theme.dart
@@ -4,7 +4,7 @@ ThemeData THEME_DATA = ThemeData(
   brightness: Brightness.light,
   primaryColor: Colors.lightBlue[800],
   textTheme: const TextTheme(
-    headline1: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+    headline1: TextStyle(fontSize: 40.0, fontWeight: FontWeight.bold),
 
 
 
